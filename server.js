a93049cbfbe30efdb6a8d9d0264e60dbbb40e16d
Kelly Cook <kellyrcook1980@gmail.com>
2021-02-27 20:26:50 -0700

    working through file structure, can't get express to recognize path to index.html

---
+++ server.js
@@ -0,0 +1,34 @@
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//start server
+app.listen(PORT, () => console.log(`Server listening on: http://localhost:${PORT}/`));
+
+//html routes
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+    console.log("I work")
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+//api routes
+
+app.post('/api/tasks', function(req, res) {
+    let newTask = req.body
+    tasksArr.push(newTask)
+    console.log(tasksArr)
+  });
+
+
